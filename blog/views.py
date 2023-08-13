457898e7164a9da75cb1cdf8e8523263aae95f8a
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-08-13 18:01:11 +0300

    Add main html-files and urls and 2 funcs in views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'admin',
+        'title': 'It''s first post',
+        'content': 'First Content',
+        'date_posted': '08.13.2023'
+    },
+    {
+        'author': 'user',
+        'title': 'It''s second post',
+        'content': 'Second Content',
+        'date_posted': '08.13.2023'
+    }
+]
+
+def home(request):
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html',  {'title': 'Page about us'})
