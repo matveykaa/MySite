ee9c61e1bd1339f6317e6c2c319c5222278c56c8
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-08-18 00:15:07 +0300

    Add user app

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostCreateView,
+    PostDetailView,
+    PostUpdateView,
+    DeletePostView
+)
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
-
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='post-delete')
 
 ]
