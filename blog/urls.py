db0c4fd241cedcadb0a7954db6754b0ddf6e7ca0
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-09-23 15:25:18 +0300

    Backend final

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import (
     PostCreateView,
     PostDetailView,
     PostUpdateView,
-    DeletePostView
+    DeletePostView,
+    UserPostListView
 )
 
 from . import views
@@ -15,6 +16,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='post-delete')
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts')
 
 ]
